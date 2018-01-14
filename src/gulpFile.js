21cb29c740aac613e97e5c66f0d5c280248a993e
tayurus <tayurus@gmail.com>
2018-01-14 20:01:00 +0300

    project' template has been created

---
+++ src/gulpFile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+
+var srcPath = {
+    htmlSrc: "html/**/*.html",
+    imgSrc: ["**/*.png", "**/*.jpg", "**/*.svg"],
+    cssSrc: ["**/*.css"],
+    jsSrc: [  "**/*.js", "!gulpFile.js", "!node_modules/**/*.js"]
+}
+
+var distSrc = {
+    dist: "../dist/",
+    distImg: "../dist/img/",
+}
+
+gulp.task('build', function() {
+    gulp.src(srcPath.htmlSrc).pipe(gulp.dest(distSrc.dist));
+
+    gulp.src(srcPath.cssSrc).pipe(rename({
+        dirname: ''
+    })).pipe(concat("styles.css")).pipe(gulp.dest(distSrc.dist));
+
+    gulp.src(srcPath.jsSrc).pipe(rename({
+        dirname: ''
+    })).pipe(concat("script.js")).pipe(gulp.dest(distSrc.dist));
+
+
+    gulp.src("fonts/**/*.*",  { base: './' }).pipe(gulp.dest("../dist/"));
+    gulp.src(srcPath.imgSrc).pipe(rename({
+        dirname: ''
+    })).pipe(gulp.dest(distSrc.distImg));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(Object.values(srcPath), ["build"]);
+});
