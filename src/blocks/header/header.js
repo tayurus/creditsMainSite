c4f420a28d452a5ba4cfdcbe4d2acab69f4c360c
tayurus <tayurus@gmail.com>
2018-01-17 18:05:53 +0300

    fix social icon and add ICO-page

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
             "textAlign": "center",
             "margin-top": "15px"
         });
-        $(".header__menu").slideToggle(500);
+        $(".header__menu").slideToggle(200);
     });
 
     $(".header__item-hasSubmenu").click(function(event) {
-        $(event.currentTarget).next().slideToggle(500);
+        $(event.currentTarget).next().slideToggle(200);
     })
 
 })
--- src/gulpFile.js
+++ src/gulpFile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var urlAdjuster = require('gulp-css-url-adjuster');
 
 var srcPath = {
     htmlSrc: "html/**/*.html",
     imgSrc: ["**/*.png", "**/*.jpg", "**/*.svg"],
-    cssSrc: ["**/*.css"],
+    cssSrc: ["**/*.css", "!node_modules/**/*.css"],
     jsSrc: [  "**/*.js", "!gulpFile.js", "!node_modules/**/*.js"]
 }
 
@@ -13,6 +14,31 @@ var distSrc = {
     dist: "../dist/",
     distImg: "../dist/img/",
 }
+
+gulp.task('buildForLeha', function() {
+    gulp.src(srcPath.htmlSrc).pipe(gulp.dest(distSrc.dist));
+
+    gulp.src(srcPath.cssSrc).pipe(rename({
+        dirname: ''
+    })).pipe(concat("styles.css")).pipe(urlAdjuster({
+        replace: ['img/', '../img/NewTemplate/img/']
+    })).pipe(urlAdjuster({
+        replace: ['fonts/', '../fonts/']
+    }))
+    .pipe(gulp.dest(distSrc.dist));
+
+
+
+    gulp.src(srcPath.jsSrc).pipe(rename({
+        dirname: ''
+    })).pipe(concat("script.js")).pipe(gulp.dest(distSrc.dist));
+
+
+    gulp.src("fonts/**/*.*",  { base: './' }).pipe(gulp.dest("../dist/"));
+    gulp.src(srcPath.imgSrc).pipe(rename({
+        dirname: ''
+    })).pipe(gulp.dest(distSrc.distImg));
+});
 
 gulp.task('build', function() {
     gulp.src(srcPath.htmlSrc).pipe(gulp.dest(distSrc.dist));
