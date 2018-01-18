d38281c2b41e23f21e7bdc97a7f01b5e2ea09606
tayurus <tayurus@gmail.com>
2018-01-19 00:59:51 +0300

    fix so many errors. Images has been convert to jpg

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -6,10 +6,18 @@ $(document).ready(function() {
             "margin-top": "15px"
         });
         $(".header__menu").slideToggle(200);
+        // $(".header__menuIcon div:nth-child(1)").hide();
+        // $(".header__menuIcon div:nth-child(2)").css({
+        //     "transform": "rotate(45deg)"
+        // });
+        // $(".header__menuIcon div:nth-child(3)").css({
+        //     "transform": "rotate(-45deg) translateX(6px) translateY(-5px)"
+        // });
     });
+
 
     $(".header__item-hasSubmenu").click(function(event) {
         $(event.currentTarget).next().slideToggle(200);
-    })
+    });
 
 })
